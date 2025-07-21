17fcecc5dbd4e988659f50fd0023c0b4aa2c2924
Noha <noha.ben254@gmail.com>
2025-07-21 13:48:58 +0200

    init pages

--- app/index.tsx
+++ app/index.tsx
@@ -7,10 +7,25 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "#fff",
         
       }}
     >
       <Text>Bloc Index :</Text>
+
+      <Text>Navigation :</Text>
+
+      <View style={{padding: 10, backgroundColor:"#c5c5c5"}}>
+          <Text>1. Login (via google)</Text>
+          <Text>a - Dashbboard </Text>
+      </View>
+            <Text>2. Register ?</Text>
+
+      <Text>Autres pages</Text>
+      
+
     </View>
+
   );
 }
+
