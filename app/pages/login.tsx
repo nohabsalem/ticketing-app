bca7226d6e72ebe02cdcf2747222098a301963c2
Noha <noha.ben254@gmail.com>
2025-07-22 11:54:52 +0200

    style + input

--- app/pages/login.tsx
+++ app/pages/login.tsx
@@ -1,18 +1,42 @@
-import { Text, View } from 'react-native';
+import { Label } from '@react-navigation/elements';
+import React, { useState } from 'react';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { styles } from "../components/ThemedText";
 
+export default function Login (){
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-export default function Login(){
-    return (
-        <View
-        style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "#fff",
-        }}
-        >
-        <Text style={styles.text01}>Login Page</Text>
-        </View>
-    );
-}
+  return (
+    <View style={styles.formcontainer}>
+      <Text style={styles.formtitle}>Connexion</Text>
+
+      <Label style={styles.label}>Email :</Label>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <Label style={styles.label}>Mot de passe :</Label>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Mot de passe"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Se connecter</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.googleButton}>
+        <Text style={styles.googleButton}>Se connecter avec Google</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
