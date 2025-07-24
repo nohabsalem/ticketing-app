613e67a6b8041fe0a77bd9eb7fd98850e90b7ed3
Noha <noha.ben254@gmail.com>
2025-07-24 15:40:21 +0200

    css

--- app/pages/dashboard.tsx
+++ app/pages/dashboard.tsx
@@ -1,12 +1,48 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Button, Text, View } from "react-native";
+import { styles } from "../components/ThemedText";
 export default function Dashboard() {
+  const ticketsOuverts = 50;
+  const ticketsFermes = 97;
+  const totalTickets = ticketsOuverts + ticketsFermes;
+
   return (
     <>
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Dashboard Page</Text>
-        <Text>TICKETS OUVERTS</Text>
-        <Text>TICKETS FERMÉS </Text>
-        <Text>TOTAL DE TICKETS </Text>
+      <View style={styles.container2}>
+        <Text style={styles.welcome}>Welcome, @user</Text>
+
+        <View style={styles.statsContainer}>
+          <View style={[styles.card, { borderColor: "#4ecdc4" }]}>
+            <Text style={styles.label}>Tickets ouverts :</Text>
+            <Text style={styles.number}>{ticketsOuverts}</Text>
+          </View>
+
+          <View style={[styles.card, { borderColor: "#ff6b6b" }]}>
+            <Text style={styles.label}>Tickets fermés :</Text>
+            <Text style={styles.number}>{ticketsFermes}</Text>
+          </View>
+        </View>
+
+        <View style={styles.totalBox}>
+          <Text style={styles.label}>Total de tickets :</Text>
+          <Text style={styles.total}>{totalTickets}</Text>
+        </View>
+
+        <View style={styles.ticketsInfo}>
+          <Text style={styles.noTicket}>• Vous n'avez ouvert aucun ticket</Text>
+          <View style={styles.button2}>
+            <Button
+              title="Créer un nouveau ticket"
+              onPress={() => console.log("Créer un ticket")}
+            />
+          </View>
+        </View>
+        <Text style={styles.footer}>
+          <Link href="/" style={styles.text01}>
+            Accueil
+          </Link>{" "}
+          © 2025 La Plateforme - Tous droits réservés
+        </Text>
       </View>
     </>
   );
