0253effdec7ef2c01f7b6e14bf81ea6fa36a88b0
Noha <noha.ben254@gmail.com>
2025-07-24 16:25:05 +0200

    nav to home

--- app/pages/login.tsx
+++ app/pages/login.tsx
@@ -1,42 +1,51 @@
-import { Label } from '@react-navigation/elements';
-import React, { useState } from 'react';
-import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Label } from "@react-navigation/elements";
+import { Link } from "expo-router";
+import React, { useState } from "react";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from "../components/ThemedText";
-
-export default function Login (){
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
-    <View style={styles.formcontainer}>
-      <Text style={styles.formtitle}>Connexion</Text>
+    <>
+      <View style={styles.formcontainer}>
+        <Text style={styles.formtitle}>Connexion</Text>
 
-      <Label style={styles.label}>Email :</Label>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-        keyboardType="email-address"
-        autoCapitalize="none"
-      />
-      <Label style={styles.label}>Mot de passe :</Label>
+        <Label style={styles.label}>Email :</Label>
+        <TextInput
+          style={styles.forminput}
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <Label style={styles.label}>Mot de passe :</Label>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Mot de passe"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
+        <TextInput
+          style={styles.forminput}
+          placeholder="Mot de passe"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
 
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Se connecter</Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Se connecter</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity style={styles.googleButton}>
-        <Text style={styles.googleButton}>Se connecter avec Google</Text>
-      </TouchableOpacity>
-    </View>
+        <TouchableOpacity style={styles.googleButton}>
+          <Text style={styles.googleButton}>Se connecter avec Google</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.footer}>
+          <Link href="/" style={styles.text01}>
+            Accueil <br />
+          </Link>{" "}
+          © 2025 La Plateforme - Tous droits réservés
+        </Text>
+      </View>
+    </>
   );
-};
+}
