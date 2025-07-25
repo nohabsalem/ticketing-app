1320beb24411d473ce7781d08ae16e8f08954c9f
Noha <noha.ben254@gmail.com>
2025-07-25 14:24:58 +0200

    basic + ugly form

--- app/pages/creating-ticket.tsx
+++ app/pages/creating-ticket.tsx
@@ -1,19 +1,53 @@
 import { Link } from "expo-router";
 import { Button, Text, TextInput, View } from "react-native";
+import RNPickerSelect from "react-native-picker-select";
+
 import { styles } from "../components/ThemedText";
+
+const options = [
+  { label: "Choisissez un sujet :", value: null },
+  { label: "Extrascolaire", value: "extrascolaire" },
+  { label: "Innovation", value: "innovation" },
+  { label: "Création compte/accès", value: "compte" },
+  { label: "Support étudiants", value: "etudiants" },
+  { label: "Support suite Google", value: "google" },
+  { label: "Support Alcasar", value: "alcasar" },
+  { label: "Marketing", value: "marketing" },
+  { label: "TUMO", value: "tumo" },
+];
+
+const priorities = [
+  { label: "Faible", value: "faible" },
+  { label: "Moyenne", value: "moyenne" },
+  { label: "Élevée", value: "élevée" },
+  { label: "Urgente", value: "urgente" },
+];
+
 export default function CreatingTicket() {
   return (
     <>
-      <View style={styles.container}>
-        <Text style={styles.title}>Créer un nouveau ticket</Text>
-        <TextInput style={styles.forminput} placeholder="Titre du ticket" />
-        <TextInput
-          style={styles.forminput}
-          placeholder="Description"
-          multiline
+      <View>
+        <Text>Créer un nouveau ticket</Text>
+
+        <TextInput placeholder="Titre du ticket : " />
+
+        <TextInput placeholder="Description" multiline />
+
+        <RNPickerSelect
+          onValueChange={(value) => console.log("Sujet choisi :", value)}
+          items={options}
+          placeholder={{ label: "Sélectionnez un sujet", value: null }}
         />
+
+        <RNPickerSelect
+          onValueChange={(value) => console.log("Priorité :", value)}
+          items={priorities}
+          placeholder={{ label: "Sélectionnez une priorité", value: null }}
+        />
+
         <Button title="Soumettre" onPress={() => console.log("Ticket créé")} />
       </View>
+
       <View>
         <Text style={styles.footer}>
           <Link href="/" style={styles.text01}>
