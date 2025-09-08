b4d3f9441d1b77517ac84c4270b5b06316aa6566
Noha <noha.ben254@gmail.com>
2025-09-08 11:08:38 +0200

    links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,11 +4,25 @@ import { Text, View } from "react-native";
 
 export default function Footer() {
   return (
-    <View className="items-center bg-blue-600 p-4 mt-8">
-      <Link href="/" className="text-lg underline text-white mb-2">
-        Accueil {"\n"}
-      </Link>
-      <Text className="text-md text-white">
+    <View className="items-center bg-blue-600 p-4 flex-shrink-0">
+      <View className="flex-row justify-center space-x-6 mb-2">
+        <Link href="/" className="text-lg underline text-white">
+          Accueil
+        </Link>
+        <Link
+          href="/pages/tickets/ticket-list"
+          className="text-lg underline text-white"
+        >
+          Tickets
+        </Link>
+        <Link href="/" className="text-lg underline text-white">
+          Accueil
+        </Link>
+        <Link href="/" className="text-lg underline text-white">
+          Accueil
+        </Link>
+      </View>
+      <Text className="text-sm text-white mt-1 text-center">
         © 2025 La Plateforme - Tous droits réservés
       </Text>
     </View>
